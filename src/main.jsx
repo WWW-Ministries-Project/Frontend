4211414d9af347bf65945b97586c969129c49106
amalisaah <100369935+amalisaah@users.noreply.github.com>
2023-07-06 14:57:52 +0000

    to be reconsidered

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import App from "./App.jsx";
+import LoginPage from "./pages/Authentication/pages/Login/LoginPage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Authentication from "./pages/Authentication/Authentication.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/authentication",
+    element: <Authentication />,
+    children:[
+      {index:true, element:LoginPage},
+      {
+        path:"/login",
+        element:<LoginPage/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
