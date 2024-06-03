8d0f7849446712628a8abd1cb44e57b15785392f
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-03 16:46:36 +0000

    banner

---
+++ src/pages/HomePage/pages/ProfileDetails/Components/Banner.jsx
@@ -0,0 +1,40 @@
+
+import PropTypes from 'prop-types';
+import coverImage from "/src/assets/images/CoverImage.jpg";
+import ProfilePic from "/src/components/ProfilePicture";
+import Button from '/src/components/Button';
+
+const Banner = (props) => {
+    return (
+        <div className="w-full relative">
+            <img src={coverImage} alt="cover Image" className="w-full h-48" />
+            <div className="absolute bottom-0 left-0 w-full h-48 flex items-center justify-between px-2">
+                <div className='flex gap-4 items-center'>
+                    <ProfilePic className="w-40 h-40 shadow" src={props.src} alt="cover Image" name={props.name} />
+                    <article>
+                        <div>{props.name || "No Name"}</div>
+                        <div>{(props.department || "No Department") + " | " + (props.position || "No Position")}</div>
+                        <div>{props.email || "No Email" + " | " + props.primary_number || "No Phone"}</div>
+                    </article>
+                </div>
+                <div>
+                    <Button value={"Edit Profile"} onClick={() => {}} className="w-full mt-2 p-2 bg-transparent h-8 border " />
+                </div>
+
+
+            </div>
+        </div>
+    );
+}
+
+Banner.propTypes = {
+    name: PropTypes.string,
+    department: PropTypes.string,
+    position: PropTypes.string,
+    email: PropTypes.string,
+    primary_number: PropTypes.string,
+    src: PropTypes.string
+
+}
+
+export default Banner;
--- src/pages/HomePage/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/HomePage/pages/ProfileDetails/ProfileDetails.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom";
-import coverImage from "/src/assets/images/CoverImage.jpg";
+import Banner from "./Components/Banner";
 
 const ProfileDetails = () => {
     const links =[{name:"Member Information",path:"info"},{name:"Assets",path:"assets"}]
@@ -7,9 +7,7 @@ const ProfileDetails = () => {
 
     return (
         <section className=" w-full h-full">
-            <div className="w-full relative">
-                <img src={coverImage} alt="cover Image" className="w-full" />
-            </div>
+            <Banner/>
             <div className="w-full flex justify-start gap-4 py-2 border-1 border-lightGray border-b-2">
                 {links.map((link, index) => (
                     <NavLink to={link.path} className="h-full cursor-pointer" style={({ isActive }) =>
