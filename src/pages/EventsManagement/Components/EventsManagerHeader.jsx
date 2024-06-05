ded39feaac3869424c3a67349dd0d9769ba027a8
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-05 15:37:04 +0000

    adding navigation to creating event

--- src/pages/EventsManagement/Components/EventsManagerHeader.jsx
+++ src/pages/EventsManagement/Components/EventsManagerHeader.jsx
@@ -1,7 +1,9 @@
 import Button from "/src/components/Button";
 import Filter from "/src/pages/HomePage/Components/reusable/Filter";
+import PropTypes from 'prop-types';
 
-const EventsManagerHeader = () => {
+
+const EventsManagerHeader = (props) => {
     return (
         <div  className="flex justify-between items-center my-4">
             <div className="flex gap-4">
@@ -11,10 +13,14 @@ const EventsManagerHeader = () => {
             <Button
                 value="Create Event"
                 className={" text-white h-10 p-2 bg-gradient-to-r from-violet-500 to-fuchsia-500 transition duration-300 hover:bg-gradient-to-l hover:scale-105"}
-                onClick={() => {}}
+                onClick={() => {props.onNavigate("/home/manage-event")}}
               />
         </div>
     );
 }
+
+EventsManagerHeader.propTypes = {
+    onNavigate: PropTypes.func,
+};
 
 export default EventsManagerHeader;
--- src/pages/EventsManagement/EventsManagement.jsx
+++ src/pages/EventsManagement/EventsManagement.jsx
@@ -1,11 +1,17 @@
 import EventsCard from "./Components/EventsCard";
 import GridWrapper from "/src/Wrappers/GridWrapper";
 import EventsManagerHeader from "./Components/EventsManagerHeader";
+import { useNavigate } from 'react-router-dom';
+
 
 const EventsManagement = () => {
+    const navigate = useNavigate();
+    const handleNavigation = (path) => {
+        navigate(path);
+    }
     return (
         <>
-            <EventsManagerHeader />
+            <EventsManagerHeader onNavigate={handleNavigation}/>
             <GridWrapper>
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => <EventsCard key={Math.random()} />)}
             </GridWrapper>
