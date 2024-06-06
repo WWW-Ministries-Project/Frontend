f31a1a527facfdbfce6312cc4961472a657eea1c
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-06 16:46:29 +0000

    filtering event

--- src/pages/EventsManagement/EventsManagement.jsx
+++ src/pages/EventsManagement/EventsManagement.jsx
@@ -8,16 +8,30 @@ import axios from "/src/axiosInstance";
 
 const EventsManagement = () => {
     const navigate = useNavigate();
-    const [events,setEvents] = useState([])
+    const [events,setEvents] = useState([]);
+    const [filter,setFilter] = useState({});
     const handleNavigation = (path) => {
         navigate(path);
+    }
+    const handleChange = (name,value) => {
+        setFilter((prev) => ({...prev,[name]:value}))
+    }
+    const handleFilter = () => {
+        if(filter.month && filter.year){
+            return axios.get(`/event/list-events?month=${filter.month}&year=${filter.year}`).then((res) => setEvents(res.data.data))
+        }else if(filter.month){
+            return axios.get(`/event/list-events?month=${filter.month}`).then((res) => setEvents(res.data.data))
+        } else if(filter.year){
+            return axios.get(`/event/list-events?year=${filter.year}`).then((res) => setEvents(res.data.data))
+        }
+        return axios.get(`/event/list-events`).then((res) => setEvents(res.data.data))
     }
     useEffect(() => {
       axios.get("/event/list-events").then((res) => setEvents(res.data.data))  
     }, [])
     return (
         <>
-            <EventsManagerHeader onNavigate={handleNavigation}/>
+            <EventsManagerHeader onNavigate={handleNavigation} onChange={handleChange} onFilter={handleFilter} />
             <GridWrapper>
                 {events.map((event) => <EventsCard event={event} key={Math.random()} onNavigate={handleNavigation} />)}
             </GridWrapper>
