96620f1710831fc25ff3bf1eae6b219709490c4d
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-07 10:38:23 +0000

    api calls to create attendance for event

--- src/pages/EventsManagement/pages/EventRegister.jsx
+++ src/pages/EventsManagement/pages/EventRegister.jsx
@@ -4,14 +4,31 @@ import PropTypes from 'prop-types';
 import InputDiv from '/src/pages/HomePage/Components/reusable/InputDiv';
 import Button from '/src/components/Button';
 import success from '/src/assets/images/success.png';
+// import axios from 'axios';
+import axios from "/src/axiosInstance.js"
 
 const EventRegister = () => {
+    const [memberDetails, setMemberDetails] = useState({ phone_number: '' });
     const [memberFound, setMemberFound] = useState(false);
+    const [loading, setLoading] = useState(false);
     const handleFindMember = () => {
-        setMemberFound("found");
+        setLoading(true)
+        axios.get("/event/search-user", { phone_number: memberDetails.phone_number }).then((res) => {
+            setLoading(false)
+            setMemberDetails(res.data.data)
+            setMemberFound("found");
+        })
     }
     const handleConfirm = () => {
-        setMemberFound("confirmed");
+        setLoading(true)
+        axios.post(`/event/sign-attendance?event_id=${memberDetails.user_id}`, { phone_number: memberDetails.primary_number, new_member: false }).then(() => {
+            setLoading(false)
+            setMemberFound("confirmed");
+        }).catch(()=>{})
+    }
+
+    const handleChange = (name, value) => {
+        setMemberDetails((prev) => ({ ...prev, [name]: value }));
     }
 
     return (
@@ -20,25 +37,25 @@ const EventRegister = () => {
                 <h2 className='H400'>Welcome to {"props.event.name"}</h2>
                 <p className='text-sma text-mainGray'>Fill the form below to capture your attendance</p>
                 <div className='w-full mt-10'>
-                    <InputDiv label="Phone number" inputClass="border border-2" type="tel" id="phone_number" placeholder="Enter your phone number" value="" onChange={() => { }} />
-                    <Button value="Next" className="w-full mt-8 text-white mt-10 h-8 border border-primaryViolet " onClick={handleFindMember} />
+                    <InputDiv label="Phone number" inputClass="border border-2" type="tel" id="phone_number" placeholder="Enter your phone number" value={memberDetails.phone_number} onChange={handleChange} />
+                    <Button value="Next" className="w-full mt-8 text-white mt-10 h-8 border border-primaryViolet " disabled={!memberDetails.phone_number} onClick={handleFindMember} loading={loading} />
                 </div>
             </div>}
-            {memberFound=="found" &&
+            {memberFound == "found" &&
                 <div>
                     <h2 className='H400'>Confirmation</h2>
                     <p className='text-sma text-mainGray'>Confirm if the details below are yours</p>
                     <div className='w-full mt-10'>
-                        <InputDiv label="Name" inputClass="border border-2 mb-4" type="tel" id="phone_number" value="" disabled={true} />
-                        <InputDiv label="Phone number" inputClass="border border-2" type="tel" id="phone_number" value="" disabled={true} />
+                        <InputDiv label="Name" inputClass="border border-2 mb-4" value={memberDetails.name} type="tel" id="phone_number" disabled={true} />
+                        <InputDiv label="Phone number" inputClass="border border-2" value={memberDetails.primary_number} type="tel" id="phone_number" disabled={true} />
                         <Button value="Confirm" className="w-full mt-8 text-white mt-10 h-8 border border-primaryViolet " onClick={handleConfirm} />
                     </div>
                 </div>}
-            {memberFound=="confirmed" &&
+            {memberFound == "confirmed" &&
                 <div>
-                    <div className='w-full flex flex-col items-center gap-4'>           
-                    <img src={success} alt="" />
-                    <h2 className='H400 text-primaryViolet'>Attendance recorded successfully</h2>
+                    <div className='w-full flex flex-col items-center gap-4'>
+                        <img src={success} alt="" />
+                        <h2 className='H400 text-primaryViolet'>Attendance recorded successfully</h2>
                     </div>
                 </div>}
 
