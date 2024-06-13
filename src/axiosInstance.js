465826ab2e4feac0ab091a93b092add56f499494
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-13 16:04:33 +0000

    fetching initial data

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -14,8 +14,13 @@ export const pictureInstance = axios.create({
   baseURL: baseUrl, // Your API base URL
   headers: {
     'Content-Type': 'multipart/form-data', // Use multipart/form-data for sending files
-    // 'Authorization': `Bearer ${getToken()}`,
+    'Authorization': `Bearer ${getToken()}`,
   },
 });
+
+export const changeAuth=(token)=>{
+    instance.defaults.headers['Authorization'] = `Bearer ${token}`;
+    pictureInstance.defaults.headers['Authorization'] = `Bearer ${token}`;
+}
 
 export default instance;
