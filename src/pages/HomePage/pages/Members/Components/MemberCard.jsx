e29e57be9641bda6aa66a54552cb9786192d89f5
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-14 09:03:45 +0000

    ui changes on member card

--- src/pages/HomePage/pages/Members/Components/MemberCard.jsx
+++ src/pages/HomePage/pages/Members/Components/MemberCard.jsx
@@ -11,7 +11,7 @@ const MemberCard = (props) => {
     return (
         <CardWrapper>
             <ProfilePic className="w-[7rem] h-[7rem] shadow border border-primaryViolet " src={props.userInfo.photo} alt="profile pic" />
-            <div className="w-full text-xs flex flex-col gap-1 p-1">
+            <div className="w-full break-all text-xs flex flex-col gap-1 p-1">
                 <div className='flex justify-between w-full'>
                     <p className='font-bold text-[1rem] '>{props.name}</p>
                     <img src={ellipse} alt="options" className='w-4 h-4 ' />
@@ -23,7 +23,7 @@ const MemberCard = (props) => {
                 <div className='flex gap-1 text-xs'>
                 <p className='text-sm '>{props.email|| "email"}</p>
                 </div>
-                <div>{props.userInfo.primary_number}</div>
+                <div>{props.userInfo.primary_number || "Phone Number"}</div>
                 <Button value={"View Profile"} onClick={() => {navigate(`/home/members/${props.id}/info`)}} className="w-full mt-2 bg-transparent h-8 border border-primaryViolet " />
             </div>
         </CardWrapper>
