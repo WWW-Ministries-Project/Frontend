c577cb696c7ddaca9256ee771ef4c9203896fe6f
ABBIW <abbiwjojo22@gmail.com>
2024-06-15 23:18:18 +0000

    Update to the Card warpper

--- src/Wrappers/GridWrapper.jsx
+++ src/Wrappers/GridWrapper.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 const GridWrapper = (props) => {
     return (
-        <div className={" bg-error grid gap-5 justify-center grid-cols-1 min-[710px]:grid-cols-2 tablet:gap-4 laptop:grid-cols-3 desktop:grid-cols-4 min-[1284px]:grid-cols-4  "+props.className }>
+        <div className={"w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-5 justify-center "+props.className }>
             {props.children}
         </div>
+
     );
 }
 
