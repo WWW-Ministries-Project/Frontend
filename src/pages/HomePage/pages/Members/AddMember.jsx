88bd0b8b807921a1efc6101d241f359bfa6bd4e6
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-15 14:00:57 +0000

    moved logic for adding an employee

--- src/pages/HomePage/pages/Members/AddMember.jsx
+++ src/pages/HomePage/pages/Members/AddMember.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
+import useState from "react-usestateref";
 import ProfilePicture from "../../../../components/ProfilePicture";
 import MembersForm from "./Components/MembersForm";
 import editIcon from "/assets/home/edit.svg";
+import { baseUrl } from "../../../Authentication/utils/helpers";
+import axios,{pictureInstance as axiosPic} from '/src/axiosInstance';
+import { addNewMember } from "./utils/apiCalls";
 
 
 const AddMember = () => {
   const [profilePic, setProfilePic] = useState({});
-  const [userValue, setUserValue] = useState({ "password": "123456", "department_id": "", "name": "", "email": "", "primary_number": "", "date_of_birth": "", "gender": "", "is_active": true, "address": "", "occupation": "", "company": "", "department_head": 0, "country": "" });
+  const [userValue, setUserValue, userValueRef] = useState({ "password": "123456", "department_id": "", "name": "", "email": "", "primary_number": "", "date_of_birth": "", "gender": "", "is_active": true, "address": "", "occupation": "", "company": "", "department_head": 0, "country": "" });
   function changePic(pic) {
     setProfilePic(() => pic);
   }
+  const handleChange = (name, value) => {
+    setUserValue((prev) => ({ ...prev, [name]: value }));
+}
+async function handleSubmit() {
+  const data = new FormData();
+  data.append("file", profilePic.picture);
+  const endpoint = "/upload";
+  const path = `${baseUrl}${endpoint}`;
+  console.log(data,"profilePic");
+
+  try {
+    const response =profilePic.picture && await axiosPic.post(path, data);
+    if (profilePic.picture && response.status === 200) {
+      const link = response.data.result.link;
+      setUserValue(prev => ({ ...prev, photo: link }));
+    }
+      setProfilePic({});
+      addNewMember(userValueRef.current); // sends data after picture link is received
+  } catch (error) {
+    console.log(error);
+  }
+}
   return (
     <section>
       <div className="font-bold text-xl">Member Information</div>
@@ -28,12 +53,12 @@ const AddMember = () => {
           className="h-[10rem] w-[10rem] outline-primaryViolet mt-3 profilePic transition-all duration-1000 mx-aut"
           textClass={"text-[32px] leading-[36px] mx-8 "}
           onChange={changePic}
-          id={"pic"}
+          id={"profilePic"}
         />
         <div className="text-sm text-[#8F95B2] mt-3">Image size must be less <br /> than 2mb, jpeg or png</div>
       </section>
 
-      <MembersForm user={userValue} edit={true} onChange={setUserValue} />
+      <MembersForm user={userValue} edit={true} onChange={handleChange} onSubmit={handleSubmit} />
     </section>
   );
 }
