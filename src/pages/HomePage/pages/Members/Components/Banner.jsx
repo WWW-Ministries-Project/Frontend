7489929c75d4650887e618be1370085b82c4b048
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-19 15:50:29 +0000

    ui for banner

--- src/pages/HomePage/pages/Members/Components/Banner.jsx
+++ src/pages/HomePage/pages/Members/Components/Banner.jsx
@@ -1,6 +1,5 @@
 
 import PropTypes from 'prop-types';
-import coverImage from "/src/assets/images/CoverImage.jpg";
 import coverImage1 from "/src/assets/CoverImage.svg";
 import Button from '/src/components/Button';
 import ProfilePic from "/src/components/ProfilePicture";
@@ -10,26 +9,27 @@ const Banner = (props) => {
         props.onClick();
     }
     return (
-        <div className="w-full relative">
-            <img src={coverImage1} alt="cover Image" className="w-full rounded-xl" />
-            <div className={"absolute px-4 bottom-0 left-0 w-full h-48 flex items-center justify-between px-2 bg-[url({coverImage1})]"}>
+        <div className="w-full h-48 relative">
+            {/* <img src={props.coverImage1} alt="cover Image" className="w-full rounded-xl" /> */}
+            <div className="absolute bottom-0 rounded-lg left-0 w-full h-full flex items-center justify-between px-4" style={{ backgroundImage: `url(${coverImage1})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
                 <div className='flex gap-4 items-center'>
-                    <ProfilePic className="w-40 h-40 shadow" src={props.src} alt="cover Image" name={props.name} editable={props.edit} id="coverPic" onChange={props.onPicChange} />
+                    <ProfilePic className="w-32 h-32 shadow" src={props.src} alt="cover Image" name={props.name} editable={props.edit} id="coverPic" onChange={props.onPicChange} />
                     <article>
                         <div>{props.name || "No Name"}</div>
                         <div>{(props.department || "No Department") + " | " + (props.position || "No Position")}</div>
-                        <div>{props.email || "No Email" + " | " + props.primary_number || "No Phone"}</div>
+                        <div>{(props.email || "No Email") + " | " + (props.primary_number || "No Phone")}</div>
                     </article>
                 </div>
-                {!props.edit &&<div>
-                    <Button value={"Edit Profile"} onClick={handleClick} className="w-full mt-2 p-2 bg-transparent h-8 border " />
-                </div>}
-
-
+                {!props.edit && (
+                    <div>
+                        <Button value="Edit Profile" onClick={handleClick} className="w-full mt-2 p-2 bg-transparent min-h-8 border" />
+                    </div>
+                )}
             </div>
         </div>
     );
-}
+};
+
 
 Banner.propTypes = {
     name: PropTypes.string,
