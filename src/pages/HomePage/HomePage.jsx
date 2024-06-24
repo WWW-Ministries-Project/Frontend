de7c565f3bac3d70195928d5a16888638367fcc4
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-24 17:50:57 +0000

    removed unused code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,16 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import useState from "react-usestateref";
+import useWindowSize from "../../CustomHooks/useWindowSize";
+import { useAuth } from "../../auth/AuthWrapper";
 import axios, { changeAuth } from "../../axiosInstance.js";
 import { getToken } from "../../utils/helperFunctions";
 import { baseUrl } from "../Authentication/utils/helpers";
 import Header from "../HomePage/Components/Header";
 import SideBar from "../HomePage/Components/SideBar";
-import NewMember from "./Components/NewMember";
-import { useAuth } from "../../auth/AuthWrapper";
-import useWindowSize from "../../CustomHooks/useWindowSize";
-import waves from "../../assets/wavescx.svg"
 
 
 function HomePage() {
-  const [userValue, setUserValue, userValueRef] = useState({ "password": "123456", "department_id": "", "name": "", "email": "", "primary_number": "", "date_of_birth": "", "gender": "", "is_active": true, "address": "", "occupation": "", "company": "", "department_head": 0, "country": "", link: "" });
   const [userStats, setUserStats] = useState({ stats: { adults: { male: 0, female: 0, total: 0 }, children: { male: 0, female: 0, total: 0 } } });
   const [displayForm, setDisplayForm] = useState(false);
   const [members, setMembers] = useState([]);
@@ -30,11 +27,6 @@ function HomePage() {
     setShow((prev) => !prev);
   }
 
-  const selectOptions = useMemo(() => {
-    return departmentData.map((department) => {
-      return { name: department.name, value: department.id };
-    });
-  })
 
   const CloseForm = () => {
     setDisplayForm(false);
@@ -69,31 +61,6 @@ function HomePage() {
   }, [updatedDepartment]);
 
 
-  //adding new users
-  function handleChange(name, value) {
-    if (name === "department_id") {
-      setUserValue((prev) => ({ ...prev, [name]: parseInt(value) }));
-    }
-    else
-      setUserValue((prev) => ({ ...prev, [name]: value }));
-  }
-  const addNewMember = () => {
-    // const addNewMember = (value) => {// moving user to parent component 
-    setLoading(true);
-    axios
-      .post(`${baseUrl}/user/register`, userValueRef.current)
-      .then((response) => {
-        setLoading(false);
-        setUserValue({ "password": "123456", "department_id": 0, "name": "", "email": "", "primary_number": "", "date_of_birth": "", "gender": "", "is_active": true, "address": "", "occupation": "", "company": "", "country": "", "last_visited": "", "member_since": "" });
-        setDisplayForm(false);
-        setMembers([response.data.data, ...members]);
-      })
-      .catch((error) => {
-        setLoading(false);
-        console.log(error);
-      });
-  }
-
   //table manipulation
   const [filter, setFilter] = useState("");
 
@@ -101,9 +68,6 @@ function HomePage() {
     // setFilter(val);
   };
 
-  const handlePictureUpdate = (obj) => {
-    setUserValue(prev => ({ ...prev, photo: obj }))
-  }
 
   return (
     <>
@@ -118,8 +82,6 @@ function HomePage() {
               <Outlet context={{ setDisplayForm, CloseForm, members, filter, setFilter, handleSearchChange, departmentData, setDepartmentData, userStats }} />
             </div>
           </main>
-
-          <NewMember CloseForm={CloseForm} userValue={userValue} onChange={handleChange} onSubmit={addNewMember} selectOptions={selectOptions} className={`animate-fadeIn transition-all ease-in-out w-[353px] duration-1500 ${displayForm ? "translate-x-0" : "translate-x-full"}`} loading={loading} disabled={!userValue.name} handlePictureUpdate={handlePictureUpdate} />
         </div>) : (<Navigate to="/login" />)}
     </>
   );
