129646a691322356e46a84b104dd0ce134a40df3
Saah Asiedu <saah.asiedu@amalitech.org>
2024-06-29 17:05:39 +0000

    registering a member for event

--- src/pages/EventsManagement/Components/SearchMember.jsx
+++ src/pages/EventsManagement/Components/SearchMember.jsx
@@ -1,26 +1,27 @@
-import InputDiv from "/src/pages/HomePage/Components/reusable/InputDiv";
-import Button from "/src/components/Button";
+import ContactInput from "@/components/ContactInput";
 import PropTypes from 'prop-types';
+import Button from "/src/components/Button";
 
-const SearchMember = ({memberDetails,handleChange,loading,handleFindMember,name}) => {
+const SearchMember = ({ memberDetails, handleChange, loading, handleFindMember, name }) => {
     return (
         <div>
-                <h2 className='H400'>Welcome to {name}</h2>
-                <p className='text-sma text-mainGray'>Fill the form below to capture your attendance</p>
-                <div className='w-full mt-10'>
-                    <InputDiv label="Phone number" inputClass="border border-2" type="tel" id="phone_number" placeholder="Enter your phone number" value={memberDetails.phone_number} onChange={handleChange} />
-                    <Button value="Next" className="w-full mt-8 text-white mt-10 h-8 border border-primaryViolet " disabled={!memberDetails.phone_number} onClick={handleFindMember} loading={loading} />
-                </div>
+            <h2 className='H400'>Welcome to {name}</h2>
+            <p className='text-sma text-mainGray'>Fill the form below to capture your attendance</p>
+            <div className='w-full mt-10'>
+                {/* <InputDiv label="Phone number" inputClass="border border-2" type="tel" id="phone_number" placeholder="Enter your phone number" value={memberDetails.phone_number} onChange={handleChange} /> */}
+                <ContactInput label="Phone number" inputClass="border border-2" type="tel" id="phone_number" placeholder="Enter your phone number" value={memberDetails.phone_number} onChange={handleChange} />
+                <Button value="Next" className="w-full mt-8 text-white mt-10 h-8 border bg-primaryViolet border-primaryViolet " disabled={!memberDetails.phone_number} onClick={handleFindMember} loading={loading} />
             </div>
+        </div>
     );
 }
 
-SearchMember.propTypes ={
+SearchMember.propTypes = {
     memberDetails: PropTypes.object,
     handleChange: PropTypes.func,
     handleFindMember: PropTypes.func,
     loading: PropTypes.bool,
-    name:PropTypes.string
+    name: PropTypes.string
 }
 
 export default SearchMember;
