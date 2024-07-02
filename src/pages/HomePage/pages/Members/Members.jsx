05dd9217ab40b5940fa619ecf183b7ed8523d752
Saah Asiedu <saah.asiedu@amalitech.org>
2024-07-02 13:49:09 +0000

    created grid skeleton

--- src/pages/HomePage/pages/Members/Members.jsx
+++ src/pages/HomePage/pages/Members/Members.jsx
@@ -4,11 +4,12 @@ import { useNavigate } from "react-router-dom";
 import useWindowSize from "../../../../CustomHooks/useWindowSize";
 import Button from "../../../../components/Button";
 import SearchBar from "../../../../components/SearchBar";
+import GridSkeleton from "../../Components/GridSkeleton";
+import GridComponent from "../../Components/reusable/GridComponent";
 import TableComponent from "../../Components/reusable/TableComponent";
 import { membersColumns } from "../../utils/helperFunctions";
 import GridAsset from "/src/assets/GridAsset";
 import TableAsset from "/src/assets/TableAssets";
-import GridComponent from "../../Components/reusable/GridComponent";
 function Members() {
   const members = useStore().members;
 
@@ -54,22 +55,6 @@ function Members() {
                 onChange={handleSearchChange}
                 id="searchMembers"
               />
-              {/* <select
-                name="filter"
-                id="filter"
-                placeholder="Filter"
-                className="h-10 bg-white rounded-md p-1 opacity-50 border border-[#f2f2f2]">
-                <option value="">Filter by</option>
-                <option value="Name">Name</option>
-                <option value="Department">Department</option>
-                <option value="Date">Date created</option>
-              </select> */}
-              {/* <select name="filter" id="filter" placeholder="Filter" className="h-10 bg-white rounded-md p-1 opacity-50 border border-[#f2f2f2]">
-                <option value="">Filter by</option>
-                <option value="Name">Name</option>
-                <option value="Department">Department</option>
-                <option value="Date">Date created</option>
-             </select> */}
             </div>
             <div>
               <Button
@@ -81,22 +66,23 @@ function Members() {
           </div>
           {/* <TableComponent /> */}
           <div className={`w-full mx-auto  ${tableView ? "bg-white p-2" : "bg-transparent "} rounded-xl`}>
-            {tableView?<TableComponent
-              columns={columns}
-              data={members}
-              displayedCount={12}
-              filter={filterMembers}
-              setFilter={setFilterMembers}
-              tableView={tableView}
-            />:
-            <GridComponent
-              columns={columns}
-              data={members}
-              displayedCount={24}
-              filter={filterMembers}
-              setFilter={setFilterMembers}
-              tableView={tableView}
-            />}
+            {!true ? <GridSkeleton />
+              : (tableView ? <TableComponent
+                columns={columns}
+                data={members}
+                displayedCount={12}
+                filter={filterMembers}
+                setFilter={setFilterMembers}
+                tableView={tableView}
+              /> :
+                <GridComponent
+                  columns={columns}
+                  data={members}
+                  displayedCount={24}
+                  filter={filterMembers}
+                  setFilter={setFilterMembers}
+                  tableView={tableView}
+                />)}
           </div>
         </section>
       </main>
