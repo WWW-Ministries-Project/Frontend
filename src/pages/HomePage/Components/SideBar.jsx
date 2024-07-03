bf888081e32b1e3afbc0bdc0754070ed6bb9b898
ABBIW <abbiwjojo22@gmail.com>
2024-07-03 00:56:06 +0000

    Update SideBar.jsx

--- src/pages/HomePage/Components/SideBar.jsx
+++ src/pages/HomePage/Components/SideBar.jsx
@@ -39,11 +39,11 @@ const SideBar = ({ show, ...props }) => {
     }
 
     return (
-        <div className="" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="mx-auto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className={`borde mx-auto px-1 lg:h-full xs:h-[6vh] xs:rounded-t-2xl lg:rounded-2xl pt-1 fixed bg-whit z-10 ${!show ? "lg:w-10 lg:min-w-[3.7vw]" : "lg:w-[15%] lg:min-w-[200px]"} transition-all duration-400 linear xs:min-h-[initial] xs:h-[70px] xs:w-full xs:bottom-0 xs:left-0 xs:flex xs:flex-row lg:flex-col  `} style={props.style}>
                 <div className={`xs:hidden lg:inline mb-10 mt-4 ${!show ? "mx-auto " : ""} xs:pb-0 xs:flex xs:items-center`}>
-                    {show ? <div className='flex gap-x-4 '><ChurchLogo show={show} /><div className='flex my-auto' onClick={handleClick}><LogoutIcon /></div></div> : <div onClick={handleClick}><div>
-                        {isHovered ? <div className='flex justify-center rounded-full  p-2 shadow-xl shadow-inner cursor-pointer hover:bg-[#6539C360]'> <LoginIcon /> </div> : <div className='flex justify-center'><ChurchLogo /></div>}
+                    {show ? <div className='flex gap-x-4 '><ChurchLogo show={show} /><div className='flex my-auto cursor-pointer' onClick={handleClick}><LogoutIcon /></div></div> : <div onClick={handleClick}><div>
+                        {isHovered ? <div className='flex justify-center rounded-xl  p-2 shadow-xl shadow-inner cursor-pointer bg-[#6539C310] border-[#6539C3] text-[#6539C3] border border-1 shadow-inner drop-shadow shadow-2xl'> <LoginIcon /> </div> : <div className='flex justify-center'><ChurchLogo /></div>}
                     </div></div>}
                 </div>
                 <div className="xs:flex lg:flex-col  justify-around xs:w-full ">
@@ -59,7 +59,7 @@ const SideBar = ({ show, ...props }) => {
                             <NavLink
                                 to={item["key"]}
                                 className={({ isActive }) =>
-                                    `hover:border-[#6539C3] hover:border hover:border-1 hover:shadow-inner hover:shadow-xl  transition h-10 z-10 flex items-center py-7 lg:my-5 ${!show ? " justify-center" : "px-2 py-7 mx-2"} my-2 rounded-xl ${isActive ? "border-[#6539C3] text-[#6539C3] border border-1 shadow-inner drop-shadow shadow-xl  transition" : "hover:text-primaryViolet"} xs:my-0 xs:flex- xs:p-2 xs:h-full`}
+                                    `hover:border-[#6539C3] hover:border hover:border-1 hover:shadow-inner hover:shadow-xl  transition h-10 z-10 flex items-center py-7 lg:my-5 ${!show ? " justify-center" : "px-2 py-7 mx-2"} my-2 rounded-xl ${isActive ? "bg-[#6539C310] border-[#6539C3] text-[#6539C3] border border-1 shadow-inner drop-shadow shadow-xl transition" : "hover:text-primaryViolet"} xs:my-0 xs:flex- xs:p-2 xs:h-full`}
                                 key={index}
                             >
                                 <IconComponent className={`${show ? "mr-2" : "min-w-[1rem] min-h-[20px]"}`} />
