4340249b63929352a4160276e83394d74139f606
Saah Asiedu <saah.asiedu@amalitech.org>
2024-07-03 13:09:45 +0000

    loader for edit employee

---
+++ src/pages/HomePage/Components/reusable/LoaderComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import loader from "@/assets/images/main_loader.gif";
+
+const LoaderComponent = () => {
+    return (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-20">
+
+        <img src={loader} alt="loader" />
+        </div>
+    );
+}
+
+export default LoaderComponent;
