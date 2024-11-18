59e66ff2f18d2662cc9f98b5f59d7001941b7e45
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2024-11-18 08:15:47 +0000

    refactor: hide members-count on small screen

--- src/pages/HomePage/pages/Members/Members.jsx
+++ src/pages/HomePage/pages/Members/Members.jsx
@@ -179,7 +179,7 @@ function Members() {
           />
         </div>
         {/* <TableComponent /> */}
-        <div className="flex gap-8 sm:flex-col md:flex-col lg:flex-row xl:flex-row w-full">
+        <div className="hidden gap-8 sm:hidden md:flex lg:flex sm:flex-col md:flex-col lg:flex-row xl:flex-row w-full">
           <MembersCount items={membersCount} />
           <MembersCount items={membersCount} />
         </div>
