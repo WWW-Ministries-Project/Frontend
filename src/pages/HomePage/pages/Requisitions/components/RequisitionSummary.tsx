7ed1f29ec9c8d4cbee1f7db85816cc60d5acd7e5
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2024-11-21 14:44:34 +0000

    convert total to 2dp

--- src/pages/HomePage/pages/Requisitions/components/RequisitionSummary.tsx
+++ src/pages/HomePage/pages/Requisitions/components/RequisitionSummary.tsx
@@ -4,49 +4,58 @@ import { getStatusColor } from "@/pages/HomePage/utils/stringOperations";
 import { DateTime } from "luxon";
 import StatusPill from "@/components/StatusPill";
 
-
-function RequisitionSummary({summary, currency}:{summary:IRequestSummary | undefined, currency:string | undefined}) {
-
-    const items = [
-        {
-            title:"Requisition id",
-            value:summary?.requisition_id
-        },
-        {
-            title:"Department",
-            value:summary?.department
-        },
-        {
-            title:"Program",
-            value:summary?.program
-        },
-        {
-            title:"Request date",
-            value:       DateTime.fromISO( summary?.request_date as string).toFormat("dd/MM/yyyy"),
-           
-        },
-        {
-            title:"Total cost",
-            value:`${currency} ${summary?.total_cost}`
-        },
-        
-    ]
+function RequisitionSummary({
+  summary,
+  currency,
+}: {
+  summary: IRequestSummary | undefined;
+  currency: string | undefined;
+}) {
+  const items = [
+    {
+      title: "Requisition id",
+      value: summary?.requisition_id,
+    },
+    {
+      title: "Department",
+      value: summary?.department,
+    },
+    {
+      title: "Program",
+      value: summary?.program,
+    },
+    {
+      title: "Request date",
+      value: DateTime.fromISO(summary?.request_date as string).toFormat(
+        "dd/MM/yyyy"
+      ),
+    },
+    {
+      title: "Total cost",
+      value: `${currency} ${summary?.total_cost?.toFixed(2)}`,
+    },
+  ];
   return (
     <aside className="border rounded-lg p-3 h-fit border-[#D9D9D9]">
-      <div className="font-semibold text-dark900"> 
-        <PageHeader title="Requisition Summary"/>
+      <div className="font-semibold text-dark900">
+        <PageHeader title="Requisition Summary" />
       </div>
-      <div className="flex flex-col gap-3"> 
-        {items.map((item)=>(
-            <div key={item.title} className="flex  whitespace-nowrap gap-3 text-left">
-                <span className="font-semibold text-dark900 ">{item.title}:</span>
-                <span className="text-left font-normal text-mainGray">{item.value}</span>
-            </div>
+      <div className="flex flex-col gap-3">
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="flex  whitespace-nowrap gap-3 text-left"
+          >
+            <span className="font-semibold text-dark900 ">{item.title}:</span>
+            <span className="text-left font-normal text-mainGray">
+              {item.value}
+            </span>
+          </div>
         ))}
-        <div  className="flex items-center  whitespace-nowrap gap-3 text-left">
-                <span className="font-semibold text-dark900 ">Status</span>
-                <StatusPill text={summary?.status as string}/>
-            </div>
+        <div className="flex items-center  whitespace-nowrap gap-3 text-left">
+          <span className="font-semibold text-dark900 ">Status</span>
+          <StatusPill text={summary?.status as string} />
+        </div>
       </div>
     </aside>
   );
