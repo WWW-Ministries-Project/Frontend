f94fd3dd7de17adc9d23d678c44f6f3e26a2a6a4
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2024-11-24 20:15:09 +0000

    refactor: action buttons

--- src/pages/HomePage/pages/Requisitions/Requisitions.tsx
+++ src/pages/HomePage/pages/Requisitions/Requisitions.tsx
@@ -7,6 +7,7 @@ import api from "@/utils/apiCalls";
 import { useFetch } from "@/CustomHooks/useFetch";
 import LoaderComponent from "../../Components/reusable/LoaderComponent";
 import ErrorPage from "@/pages/ErrorPage";
+import EmptyState from "@/components/EmptyState";
 const Requisitions = () => {
     const navigate = useNavigate()
     const {data,loading,error} = useFetch(api.fetch.fetchRequisitions,)
@@ -15,8 +16,9 @@ const Requisitions = () => {
     <PageOutline>
       <PageHeader title="Requisition" buttonValue="Request item" onClick={()=>navigate('my_requests/request')} />
      
-      {error && <ErrorPage />}
-      {loading && <LoaderComponent />}
+      {/* {error && <ErrorPage />} */}
+      {loading && <LoaderComponent  />}
+      {!loading && !data?.data?.data?.length && <EmptyState msg="No requistions found" className="flex items-center justify-center flex-col"/>}
       {/* @ts-ignore */}
       {!loading && ! error && <TableComponent columns={tableColumns} data={data?.data?.data ??[]} displayedCount={10}  />}
     </PageOutline>
