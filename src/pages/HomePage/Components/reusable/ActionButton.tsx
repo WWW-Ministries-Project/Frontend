5cdadcf6c647239201161875725113a1deda19e6
Saah Asiedu <saah.asiedu@amalitech.org>
2024-12-06 14:40:34 +0000

    users page

---
+++ src/pages/HomePage/Components/reusable/ActionButton.tsx
@@ -0,0 +1,26 @@
+import Action from "@/components/Action";
+import Elipse from "/src/assets/ellipse.svg";
+
+interface ActionButtonProps {
+  showOptions: boolean;
+  onDelete: () => void;
+  onView: () => void;
+  onEdit: () => void;
+}
+
+const ActionButton = (props: ActionButtonProps) => {
+  return (
+    <>
+      <img src={Elipse} alt="options" className="cursor-pointer" />
+      {props.showOptions && (
+        <Action className="absolute right-0 bottom-50%"
+          onDelete={props.onDelete}
+          onView={props.onView}
+          onEdit={props.onEdit}
+        />
+      )}
+    </>
+  );
+};
+
+export default ActionButton;
--- src/pages/HomePage/pages/Users/UserManagement.tsx
+++ src/pages/HomePage/pages/Users/UserManagement.tsx
@@ -1,20 +1,147 @@
-import Button from "@/components/Button";
-import TableComponent from "../../Components/reusable/TableComponent";
-import { usersColumns } from "./utils/helperFunctions";
-import { useStore } from "@/store/useStore";
+//@ts-nocheck
+import ProfilePicture from "@/components/ProfilePicture";
+import { useFetch } from "@/CustomHooks/useFetch";
+import api from "@/utils/apiCalls";
+import { ColumnDef } from "@tanstack/react-table";
+import { useState } from "react";
 import PageHeader from "../../Components/PageHeader";
 import PageOutline from "../../Components/PageOutline";
-const UserManagement = () => {
-    const users = useStore().members;
-    return (
-        <PageOutline>
-            <PageHeader buttonValue="Add User"  onClick={() => alert("delete")}/>
-            <section>
-                {/* @ts-ignore */}
-                <TableComponent columns={usersColumns} data={users} />
-            </section>
-        </PageOutline>
-    );
+import ActionButton from "../../Components/reusable/ActionButton";
+import TableComponent from "../../Components/reusable/TableComponent";
+import { UserType } from "../Members/utils/membersInterfaces";
+
+interface User extends UserType {
+  is_active: boolean;
+  permission: string;
 }
+const UserManagement = () => {
+  const { data } = useFetch(api.fetch.fetchAllMembers, { is_user: "true" });
+
+  const [selectedId, setSelectedId] = useState<number | string>("");
+
+  const handleShowOptions = (id: number | string) => {
+    setSelectedId((prevSelectedId) => (prevSelectedId === id ? "" : id));
+  };
+
+  const usersColumns: ColumnDef<User>[] = [
+    {
+      header: "Member ID",
+      cell: ({ row }) => <span>SC-24</span>,
+    },
+    {
+      header: "Name",
+      accessorKey: "name",
+      cell: ({ row }) => (
+        <div
+          className="flex items-center gap-2 cursor-pointer"
+          onClick={() => {
+            window.location.href = `/home/members/${row.original.id}/info`;
+          }}
+        >
+          <ProfilePicture
+            src={row.original.photo}
+            name={row.original.name}
+            alt="profile pic"
+            className={`h-[38px] w-[38px] rounded-full border ${
+              row.original.is_active ? "border-green" : "border-error"
+            }`}
+            textClass="font-great-vibes overflow-hidden opacity-60"
+          />{" "}
+          {row.original.name}
+        </div>
+      ),
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Department",
+      accessorKey: "department",
+      accessorFn: (row) => row.department?.name || "N/A",
+    },
+    {
+      header: "Role",
+      accessorKey: "permission",
+    },
+    {
+      header: "Acount Status",
+      accessorKey: "is_active",
+      cell: (info) => (
+        <div
+          className={
+            info.getValue()
+              ? "bg-green text-sm h-6 w-fit p-2 flex items-center justify-center rounded-lg text-center text-white "
+              : "bg-neutralGray text-sm h-6 w-fit p-2 flex items-center justify-center rounded-lg text-center text-lighterBlack"
+          }
+        >
+          {info.getValue() ? "Active" : "Inactive"}
+        </div>
+      ),
+    },
+    {
+      header: "Actions",
+      accessorKey: "actions",
+      cell: ({ row }) => (
+        <div onClick={() => handleShowOptions(row.original.id)}>
+          <ActionButton 
+            showOptions={row.original.id == selectedId}
+            onView={() => {}}
+            onEdit={() => {}}
+            onDelete={() => {}}
+          />
+        </div>
+      ),
+    },
+  ];
+
+//   const users = data?.data?.data || [];
+  const users: User[] = [
+    {
+      id: "1",
+      name: "John Doe",
+      email: "john.doe@example.com",
+      department: { name: "Engineering", id: "1" },
+      permission: "Admin",
+      is_active: true,
+      photo: "https://via.placeholder.com/150",
+    },
+    {
+      id: "2",
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      department: { name: "Marketing" },
+      permission: "Editor",
+      is_active: false,
+      photo: "https://via.placeholder.com/150",
+    },
+    {
+      id: "3",
+      name: "Alice Johnson",
+      email: "alice.johnson@example.com",
+      department: { name: "Finance" },
+      permission: "Viewer",
+      is_active: true,
+      photo: "https://via.placeholder.com/150",
+    },
+    {
+      id: "4",
+      name: "Bob Brown",
+      email: "bob.brown@example.com",
+      department: { name: "Human Resources" },
+      permission: "Admin",
+      is_active: false,
+      photo: "https://via.placeholder.com/150",
+    },
+  ];
+  ;
+
+  return (
+    <PageOutline>
+      <PageHeader title={`Users(${users.length})`} />
+      <TableComponent columns={usersColumns} data={users} />
+    </PageOutline>
+  );
+};
 
 export default UserManagement;
