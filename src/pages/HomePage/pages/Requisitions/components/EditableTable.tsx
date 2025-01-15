4da9104ea5736f5fa4d914bc4dc94cb980a82292
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2025-01-15 21:41:56 +0000

    refactor: editable

--- src/pages/HomePage/pages/Requisitions/components/EditableTable.tsx
+++ src/pages/HomePage/pages/Requisitions/components/EditableTable.tsx
@@ -1,5 +1,51 @@
 import React, { useEffect } from "react";
 import { useStore } from "@/store/useStore";
+import DeleteIcon from "@/assets/DeleteIcon";
+import Button from "@/components/Button";
+
+const TableHeader = ({ header }: { header: string }) => {
+  return (
+    <th className="border border-[#D9D9D9] px-2 py-1 text-left">{header}</th>
+  );
+};
+
+const TableData = ({
+  children,
+  className,
+  colSpan,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  colSpan?: number;
+}) => {
+  return (
+    <td
+      className={`border border-[#D9D9D9] px-2 py-1 ${className}`}
+      colSpan={colSpan}
+    >
+      {children}
+    </td>
+  );
+};
+
+interface InputProps {
+  type: string;
+  value: string | number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled: boolean;
+}
+const TableInput = ({ type, value, onChange, disabled }: InputProps) => {
+  return (
+    <input
+      type={type}
+      className="w-full bg-inherit border-none outline-none rounded px-2 py-1"
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+    />
+  );
+};
+
 interface TableRow {
   name: string;
   quantity: number;
@@ -38,91 +84,77 @@ const EditableTable: React.FC<EditableTableProps> = ({
     updateRow(index, field, value);
   };
 
+  const totalSum = rows.reduce((sum, row) => sum + row.total, 0);
+
   return (
     <div className="p-4">
       {isEditable && (
-        <div
-          className="mb-4 font-bold text-primaryViolet cursor-pointer float-right"
+        <Button
+          value="+ Add item"
+          className="font-bold text-primaryViolet cursor-pointer float-right"
           onClick={addRow}
-        >
-          {" "}
-          + Add item
-        </div>
+        />
       )}
-      {rows?.length>0 &&  <table className="min-w-full border-collapse border border-[#D9D9D9]">
+      {rows?.length > 0 && (
+        <table className="min-w-full border-collapse border border-[#D9D9D9]">
           <thead>
             <tr className="bg-[#F2F4F7]">
-              <th className="border border-[#D9D9D9] px-2 py-1 text-left">
-                Name
-              </th>
-              <th className="border border-[#D9D9D9] px-2 py-1 text-left">
-                Quanity
-              </th>
-              <th className="border border-[#D9D9D9] px-2 py-1 text-left">
-                Amount
-              </th>
-              <th className="border border-[#D9D9D9] px-2 py-1 text-left">
-                Total
-              </th>
-              {isEditable && (
-                <th className="border border-[#D9D9D9] px-2 py-1 text-left">
-                  Remove
-                </th>
-              )}
+              <TableHeader header="Name" />
+              <TableHeader header="Quantity" />
+              <TableHeader header="Amount" />
+              <TableHeader header="Total" />
+
+              {isEditable && <TableHeader header="Remove" />}
             </tr>
           </thead>
           <tbody>
             {rows.map((row, index) => (
-              <tr key={index} className="odd:bg-white even:bg-[#F2F4F7]">
-                <td className="border border-[#D9D9D9] px-2 py-1">
-                  <input
+              <tr key={row.id} className="odd:bg-white even:bg-[#F2F4F7]">
+                <TableData>
+                  <TableInput
                     type="text"
-                    className="w-full bg-inherit border-none outline-none rounded px-2 py-1"
                     value={row.name}
                     onChange={(e) =>
                       handleInputChange(index, "name", e.target.value)
                     }
                     disabled={!isEditable}
                   />
-                </td>
-                <td className="border border-[#D9D9D9] px-2 py-1">
-                  <input
+                </TableData>
+                <TableData>
+                  <TableInput
                     type="number"
-                    className="w-full bg-inherit border-none outline-none rounded px-2 py-1"
                     value={row.quantity}
                     onChange={(e) =>
                       handleInputChange(index, "quantity", e.target.value)
                     }
                     disabled={!isEditable}
                   />
-                </td>
-                <td className="border border-[#D9D9D9] px-2 py-1">
-                  <input
+                </TableData>
+                <TableData>
+                  <TableInput
                     type="number"
-                    className="w-full bg-inherit border-none outline-none rounded px-2 py-1"
                     value={row.amount}
                     onChange={(e) =>
                       handleInputChange(index, "amount", e.target.value)
                     }
                     disabled={!isEditable}
                   />
-                </td>
-                <td className="border border-[#D9D9D9] px-2 py-1">{row.total}</td>
+                </TableData>
+                <TableData>{row.total}</TableData>
                 {isEditable && (
-                  <td className="border border-[#D9D9D9] px-2 py-1">
-                    <button
-                      className="text-red-500 hover:text-red-700"
-                      onClick={() => deleteRow(index)}
-                      disabled={!isEditable}
-                    >
-                      Delete
-                    </button>
-                  </td>
+                  <TableData className="mt-2 flex items-center justify-center border-none">
+                    <DeleteIcon onClick={() => deleteRow(index)} />
+                  </TableData>
                 )}
               </tr>
             ))}
+            <tr >
+              <TableData colSpan={3}>Total</TableData>
+              <TableData>{totalSum.toFixed(2)}</TableData>
+            </tr>
           </tbody>
-        </table>}
+        </table>
+      )}
     </div>
   );
 };
