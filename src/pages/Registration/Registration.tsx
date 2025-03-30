8443ad354ac0f7e3a441825bb75f201f2e0417a8
Saah Asiedu <saah.asiedu@amalitech.org>
2025-03-30 17:50:05 +0000

    update

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,6 +1,7 @@
 //TODO: UPDATE THIS AFTER DEMO
 
 import { pictureInstance as axiosPic } from "@/axiosInstance";
+import ChurchLogo from "@/components/ChurchLogo";
 import { usePost } from "@/CustomHooks/usePost";
 import { Stepper } from "@/pages/Registration/components/Stepper";
 import api from "@/utils/apiCalls";
@@ -78,53 +79,66 @@ const Registration = () => {
   }
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values) => handleSubmit(values)}
-      validationSchema={
-        getValidationSchema(initialValues.personal_info.has_children)[
-          currentStep
-        ]
-      }
-    >
-      {(formik: FormikProps<IRegistration>) => {
-        const steps = getSteps(formik.values.personal_info.has_children);
+    <div className="bg-[url('https://res.cloudinary.com/akwaah/image/upload/v1740860331/background_oswjfy.jpg')] bg-no-repeat bg-right bg-cover h-screen flex items-center">
+      <div className="bg-white w-2/3 max-w-[1000px] h-[90%] mx-auto overflow-y-scroll rounded-lg">
+        <div className="flex flex-col items-center">
+          <ChurchLogo className={" mb-5 "} show={true} />
+          <h2 className="p-1 text-sm md:text-2xl font-bold mb-4">
+            Welcome to our registration portal
+          </h2>
+          <p className="md:text-2xl mb-4">Let's get started</p>
+        </div>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values) => handleSubmit(values)}
+          validationSchema={
+            getValidationSchema(initialValues.personal_info.has_children)[
+              currentStep
+            ]
+          }
+        >
+          {(formik: FormikProps<IRegistration>) => {
+            const steps = getSteps(formik.values.personal_info.has_children);
 
-        // Reset 'children' when 'has_children' is false
-        useEffect(() => {
-          if (!formik.values.personal_info.has_children) {
-            formik.setFieldValue("children", undefined);
-          } else if (!formik.values.children) {
-            formik.setFieldValue(
-              "children",
-              ChildrenSubForm.initialValues.children
+            // Reset 'children' when 'has_children' is false
+            useEffect(() => {
+              if (!formik.values.personal_info.has_children) {
+                formik.setFieldValue("children", undefined);
+              } else if (!formik.values.children) {
+                formik.setFieldValue(
+                  "children",
+                  ChildrenSubForm.initialValues.children
+                );
+              }
+            }, [formik.values.personal_info.has_children]);
+
+            const handleNext = async () => {
+              const errors: any = await formik.validateForm();
+              if (Object.keys(errors).length > 0) {
+                formik.setTouched(errors);
+                console.log("Validation failed:", errors);
+                return;
+              }
+              setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1));
+              if (currentStep === steps.length - 1) {
+                formik.submitForm();
+              }
+            };
+
+            return (
+              <Stepper
+                steps={steps}
+                currentStep={currentStep}
+                handleNext={handleNext}
+                handleBack={() =>
+                  setCurrentStep((prev) => Math.max(0, prev - 1))
+                }
+              />
             );
-          }
-        }, [formik.values.personal_info.has_children]);
-
-        const handleNext = async () => {
-          const errors: any = await formik.validateForm();
-          if (Object.keys(errors).length > 0) {
-            formik.setTouched(errors);
-            console.log("Validation failed:", errors);
-            return;
-          }
-          setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1));
-          if (currentStep === steps.length - 1) {
-            formik.submitForm();
-          }
-        };
-
-        return (
-          <Stepper
-            steps={steps}
-            currentStep={currentStep}
-            handleNext={handleNext}
-            handleBack={() => setCurrentStep((prev) => Math.max(0, prev - 1))}
-          />
-        );
-      }}
-    </Formik>
+          }}
+        </Formik>
+      </div>
+    </div>
   );
 };
 
@@ -138,7 +152,7 @@ const initialValues: IRegistration = {
   personal_info: UserSubForm.initialValues,
   work_info: WorkInfoSubForm.initialValues,
   status: "UNCONFIRMED",
-  church_info:{membership_type: "ONLINE"},
+  church_info: { membership_type: "ONLINE" },
   //@ts-expect-error
   children: undefined,
   ...RegistrationContactSubForm.initialValues,
