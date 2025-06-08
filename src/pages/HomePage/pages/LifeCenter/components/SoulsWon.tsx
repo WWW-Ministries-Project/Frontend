8a0a7217f10b9af84abaa9d2aa28978b5724fc76
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2025-06-08 21:08:56 +0000

    feature: delete soul

--- src/pages/HomePage/pages/LifeCenter/components/SoulsWon.tsx
+++ src/pages/HomePage/pages/LifeCenter/components/SoulsWon.tsx
@@ -1,11 +1,82 @@
 import TableComponent from "@/pages/HomePage/Components/reusable/TableComponent";
 import { HeaderControls } from "@/components/HeaderControls";
 import { SoulsWonType } from "@/utils/api/lifeCenter/interfaces";
-import { tableColumns } from "../utils/tableColumn";
+import { ColumnDef } from "@tanstack/react-table";
+import ActionButton from "@/pages/HomePage/Components/reusable/ActionButton";
+import { useMemo, useState } from "react";
+import { useDelete } from "@/CustomHooks/useDelete";
+import { api } from "@/utils/api/apiCalls";
+import { showDeleteDialog, showNotification } from "@/pages/HomePage/utils";
 
-export function SoulsWon({ soulsWon }: { soulsWon: SoulsWonType[] }) {
+interface Props {
+  soulsWon: SoulsWonType[];
+  setSoulsWon: React.Dispatch<React.SetStateAction<SoulsWonType[]>>;
+}
+
+export function SoulsWon({ soulsWon, setSoulsWon }: Props) {
+  const [selectedId, setSelectedId] = useState<number | string>("");
+
+  const { executeDelete } = useDelete(api.delete.deleteSoulWon);
+
+  const handleShowOptions = (id: number | string) => {
+    setSelectedId((prev) => (prev === id ? "" : id));
+  };
+
+  const deleteSoul = (id: string, name: string) => {
+    showDeleteDialog({ id, name }, async () => {
+      await executeDelete({ id });
+      setSoulsWon((prev) => prev.filter((soul) => soul.id !== id));
+      showNotification("Soul deleted successfully", "success");
+    });
+  };
+
+  const tableColumns: ColumnDef<SoulsWonType>[] = useMemo(
+    () => [
+      {
+        header: "NAME",
+        cell: ({ row }) => {
+          const name = `${row.original.first_name} ${row.original.last_name}`;
+          return <div>{name}</div>;
+        },
+      },
+      {
+        header: "CONTACT",
+        accessorKey: "contact_number",
+      },
+      {
+        header: "LOCATION",
+        accessorKey: "country",
+      },
+      {
+        header: "DATE WON",
+        accessorKey: "date_won",
+      },
+      {
+        header: "WON BY",
+        accessorKey: "wonByName",
+      },
+      {
+        header: "Action",
+        cell: ({ row }) => {
+          const name = `${row.original.first_name} ${row.original.last_name}`;
+          return (
+            <div onClick={() => handleShowOptions(row.original.id)}>
+              <ActionButton
+                showOptions={row.original.id === selectedId}
+                onDelete={() => deleteSoul(row.original.id, name)}
+                onEdit={() => {}}
+                onView={() => {}}
+              />
+            </div>
+          );
+        },
+      },
+    ],
+    [deleteSoul]
+  );
+
   return (
-    <div>
+    <>
       <HeaderControls
         title={`Souls won (${soulsWon.length})`}
         subtitle=""
@@ -19,6 +90,6 @@ export function SoulsWon({ soulsWon }: { soulsWon: SoulsWonType[] }) {
         data={soulsWon}
         displayedCount={10}
       />
-    </div>
+    </>
   );
 }
--- src/pages/HomePage/pages/LifeCenter/pages/ViewLifeCenter.tsx
+++ src/pages/HomePage/pages/LifeCenter/pages/ViewLifeCenter.tsx
@@ -6,67 +6,64 @@ import { SoulsWon } from "../components/SoulsWon";
 import PageOutline from "@/pages/HomePage/Components/PageOutline";
 import VisitorIcon from "@/assets/sidebar/VisitorIcon";
 import { useEffect, useState } from "react";
-import { SoulsWonType } from "@/utils";
+import { SoulsWonType } from "@/utils/api/lifeCenter/interfaces";
 
 export function ViewLifeCenter() {
-  const { id } = useParams();
+  const { id: lifeCenterId } = useParams();
+  const id = decodeQuery(String(lifeCenterId));
+  const { data } = useFetch(api.fetch.fetchLifeCenterById, { id });
 
-  const LCId = decodeQuery(String(id));
-  const { data } = useFetch(api.fetch.fetchLifeCenterById, {
-    id: LCId,
-  });
-  const [soulsWon, setSoulsWon] = useState<SoulsWonType[]>([])
+  const lifeCenterData = data?.data;
 
-  const LCData = data?.data;
+  const [souls, setSouls] = useState<SoulsWonType[]>([]);
 
-  useEffect(()=>{
-    if(LCData){
-        setSoulsWon(LCData.soulsWon)
+  useEffect(() => {
+    if (lifeCenterData?.soulsWon) {
+      setSouls(lifeCenterData.soulsWon);
     }
-  },[LCData])
+  }, [lifeCenterData?.soulsWon]);
 
   return (
     <PageOutline>
-      
-        <div className="space-y-5">
-          <div className="bg-primary text-white p-4 rounded-t-lg">
-            <div className="space-y-1">
-              <p className=" text-2xl font-semibold">{LCData?.name || "No name"}</p>
-              <p>{LCData?.description || "No description"}</p>
-            </div>
-
-            <div className="flex gap-5 items-center justify-between mt-3 pr-5  ">
-              <div className="flex items-center gap-2">
-                <img src="/src/assets/location.svg" className="size-5" />
-                <p>{LCData?.location || "No location"}</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <img src="/src/assets/calendar.svg" className="size-5" />
-                <ul className="border flex divide-x-[1px] w-fit">
-                  {LCData?.meeting_dates?.map((date, index) => (
-                    <li key={index} className="px-2">
-                      {date.slice(0, 3)}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="flex items-center gap-2">
-                <img src="/src/assets/member.svg" className="size-5" />
-                <p>{LCData?.num_of_members || 0} Members</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <VisitorIcon />
-
-                <p>{LCData?.num_of_souls_won || 0} Souls won</p>
-              </div>
-            </div>
+      <div className="space-y-5">
+        <div className="bg-primary text-white p-4 rounded-t-lg">
+          <div className="space-y-1">
+            <p className="text-2xl font-semibold">
+              {lifeCenterData?.name || "No name"}
+            </p>
+            <p>{lifeCenterData?.description || "No description"}</p>
           </div>
 
-          <div className="border border-lightGray rounded-lg p-4">
-            <SoulsWon soulsWon={soulsWon} />
+          <div className="flex gap-5 items-center justify-between mt-3 pr-5">
+            <div className="flex items-center gap-2">
+              <img src="/src/assets/location.svg" className="size-5" />
+              <p>{lifeCenterData?.location || "No location"}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <img src="/src/assets/calendar.svg" className="size-5" />
+              <ul className="border flex divide-x-[1px] w-fit">
+                {lifeCenterData?.meeting_dates?.map((date, index) => (
+                  <li key={index} className="px-2">
+                    {date.slice(0, 3)}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex items-center gap-2">
+              <img src="/src/assets/member.svg" className="size-5" />
+              <p>{lifeCenterData?.totalMembers || 0} Members</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <VisitorIcon />
+              <p>{souls.length} Souls won</p>
+            </div>
           </div>
         </div>
-      
+
+        <div className="border border-lightGray rounded-lg p-4">
+          <SoulsWon soulsWon={souls} setSoulsWon={setSouls} />
+        </div>
+      </div>
     </PageOutline>
   );
 }
