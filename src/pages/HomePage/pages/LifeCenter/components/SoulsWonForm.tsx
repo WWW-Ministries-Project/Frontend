2004864b0fc7167be9845a0eac087de613f09e4f
Saah Asiedu <saah.asiedu@amalitech.org>
2025-06-10 16:51:38 +0000

    souls form updated

--- src/pages/HomePage/pages/LifeCenter/components/SoulsWonForm.tsx
+++ src/pages/HomePage/pages/LifeCenter/components/SoulsWonForm.tsx
@@ -1,19 +1,18 @@
-import { FormikInputDiv } from "@/components/FormikInputDiv";
-import { Formik, Form, Field } from "formik";
 import { Button } from "@/components";
-import { SoulsWonType } from "@/utils";
-import { FormLayout } from "@/components/ui";
-import { object,string } from "yup";
+import { FormikInputDiv } from "@/components/FormikInputDiv";
+import { FormHeader, FormLayout } from "@/components/ui";
+import { Field, Form, Formik } from "formik";
+import { object, string } from "yup";
 
 interface IProps {
-  onSubmit: (values: SoulsWonType) => void;
+  onSubmit: (values: ISoulsWonForm) => void;
   onClose: () => void;
 }
 export const SoulsWonForm = ({ onSubmit, onClose }: IProps) => {
   return (
     <Formik
       initialValues={initialValues}
-      validationSchema={soulsWonSchema}
+      validationSchema={validationSchema}
       onSubmit={(values, { resetForm }) => {
         onSubmit(values);
         resetForm();
@@ -21,69 +20,66 @@ export const SoulsWonForm = ({ onSubmit, onClose }: IProps) => {
     >
       {({ isSubmitting, handleSubmit }) => (
         <Form className="space-y-6 w-[90vw] sm:w-[70vw] xl:w-[50vw] p-6">
-          <h3 className="text-lg font-semibold">Add a Soul</h3>
-
-          <div>
-            <FormLayout>
-              <Field
-                name="first_name"
-                component={FormikInputDiv}
-                label="First Name"
-                id="first_name"
-                placeholder="Enter first name"
-              />
-              <Field
-                name="last_name"
-                component={FormikInputDiv}
-                label="Last Name"
-                id="last_name"
-                placeholder="Enter last name"
-              />
-              <Field
-                name="contact_number"
-                component={FormikInputDiv}
-                label="Contact"
-                id="contact"
-                placeholder="Enter phone number"
-              />
-              <Field
-                name="contact_email"
-                component={FormikInputDiv}
-                label="Email"
-                id="email"
-                placeholder="Email"
-              />
-              <Field
-                name="country"
-                component={FormikInputDiv}
-                label="Country"
-                id="location"
-                placeholder="Enter country"
-              />
-              <Field
-                name="city"
-                component={FormikInputDiv}
-                label="City"
-                id="city"
-                placeholder="Enter city"
-              />
-              <Field
-                type="date"
-                name="date_won"
-                component={FormikInputDiv}
-                label="Date Won"
-                id="date_won"
-                placeholder="Select date"
-              />
-              <Field
-                name="won_by"
-                component={FormikInputDiv}
-                label="Won By"
-                id="won_by"
-                placeholder="Enter name of person who won the soul"
-              />
-            </FormLayout>
-          </div>
+          <FormLayout>
+            <FormHeader>Add a Soul</FormHeader>
+            <Field
+              name="first_name"
+              component={FormikInputDiv}
+              label="First Name"
+              id="first_name"
+              placeholder="Enter first name"
+            />
+            <Field
+              name="last_name"
+              component={FormikInputDiv}
+              label="Last Name"
+              id="last_name"
+              placeholder="Enter last name"
+            />
+            <Field
+              name="contact_number"
+              component={FormikInputDiv}
+              label="Contact"
+              id="contact"
+              placeholder="Enter phone number"
+            />
+            <Field
+              name="contact_email"
+              component={FormikInputDiv}
+              label="Email"
+              id="email"
+              placeholder="Email"
+            />
+            <Field
+              name="country"
+              component={FormikInputDiv}
+              label="Country"
+              id="location"
+              placeholder="Enter country"
+            />
+            <Field
+              name="city"
+              component={FormikInputDiv}
+              label="City"
+              id="city"
+              placeholder="Enter city"
+            />
+            <Field
+              type="date"
+              name="date_won"
+              component={FormikInputDiv}
+              label="Date Won"
+              id="date_won"
+              placeholder="Select date"
+            />
+            <Field
+              name="won_by"
+              component={FormikInputDiv}
+              label="Won By"
+              id="won_by"
+              placeholder="Enter name of person who won the soul"
+            />
+          </FormLayout>
 
           <div className="flex items-center justify-end gap-3">
             <Button
@@ -106,8 +102,18 @@ export const SoulsWonForm = ({ onSubmit, onClose }: IProps) => {
     </Formik>
   );
 };
-
-const initialValues: SoulsWonType = {
+export interface ISoulsWonForm {
+  first_name: string;
+  last_name: string;
+  contact_number: string;
+  contact_email: string;
+  country: string;
+  city: string;
+  date_won: string;
+  won_by: string;
+  id?: string;
+}
+const initialValues: ISoulsWonForm = {
   first_name: "",
   last_name: "",
   contact_number: "",
@@ -119,13 +125,13 @@ const initialValues: SoulsWonType = {
   id: "",
 };
 
-const soulsWonSchema = object().shape({
-  first_name: string().required("First name is required"),
-  last_name: string().required("Last name is required"),
-  contact_email: string().email().required("Email is required"),
-  contact_number: string().required("Contact is required"),
-  country: string().required("Country is required"),
-  city: string().required("City is required"),
-  date_won: string().required("Date won is required"),
-  won_by: string().required("Won by is required"),
+const validationSchema = object().shape({
+  first_name: string().required("required"),
+  last_name: string().required("required"),
+  contact_email: string().email().required("required"),
+  contact_number: string().required("required"),
+  country: string().required("required"),
+  city: string().required("required"),
+  date_won: string().required("required"),
+  won_by: string().required("required"),
 });
