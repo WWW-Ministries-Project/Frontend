2698c982d779b6fdbdf77acf435672f7c5a351c4
Saah Asiedu <saah.asiedu@amalitech.org>
2025-06-13 21:54:28 +0000

    editted members and settings store

--- src/pages/HomePage/pages/Members/utils/membersInterfaces.ts
+++ src/pages/HomePage/pages/Members/utils/membersInterfaces.ts
@@ -20,48 +20,6 @@ export interface UserType {
   title: string;
   photo: string;
 }
-// export interface UserType {
-//   is_active: boolean;
-//   user_info?: any; //todo : get correct type
-//   id: number | string;
-//   is_user?: boolean;
-//   title?: string;
-//   name: string;
-//   first_name?: string;
-//   other_name?: string;
-//   last_name?: string;
-//   date_of_birth?: string;
-//   gender?: string;
-//   marital_status?: string;
-//   primary_number: string;
-//   country_code?: string;
-//   email: string;
-//   address?: string;
-//   country?: string;
-//   membership_type?: string;
-//   photo?: string;
-//   department?: { id: string; name: string };
-//   position?: { id: string; name: string };
-//   position_id?: string;
-//   department_id?: string;
-//   work_name?: string;
-//   work_industry?: string;
-//   work_position?: string;
-//   emergency_contact_name?: string;
-//   emergency_contact_relation?: string;
-//   emergency_contact_phone_number?: string;
-//   status:string;
-//   work_info?: {
-//     name_of_institution: string;
-//     industry: string;
-//     position: string;
-//   };
-//   emergency_contact?: {
-//     name: string;
-//     relation: string;
-//     phone_number: string;
-//   };
-// }
 type statsType = {
   total_members: number;
   total_males: number;
@@ -84,7 +42,7 @@ export interface MemberSlice {
   removeMember: (memberId: number | string) => void;
   updateMember: (updatedMember: UserType) => void;
   setMembers: (members: UserType[]) => void;
-  setMemberOptions: (memberOptions: ISelectOption[]) => void;
+  setMemberOptions: () => void;
   setUserStats: (userStats: UserStats) => void;
 }
 
