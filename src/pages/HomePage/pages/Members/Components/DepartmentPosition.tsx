229a4731b3ce6e9ec1fe6a6b9d77c87871a6ad1c
Saah Asiedu <saah.asiedu@amalitech.org>
2025-06-22 14:30:31 +0000

    departmentPositions

--- src/pages/HomePage/pages/Members/Components/DepartmentPosition.tsx
+++ src/pages/HomePage/pages/Members/Components/DepartmentPosition.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components";
 import FormikSelectField from "@/components/FormikSelect";
 import { FullWidth } from "@/components/ui";
+import HorizontalLine from "@/pages/HomePage/Components/reusable/HorizontalLine";
 import { Field, FieldArray, getIn, useFormikContext } from "formik";
-import { useMemo } from "react";
+import { Fragment, useMemo } from "react";
 import { array, object, string } from "yup";
 import useSettingsStore from "../../Settings/utils/settingsStore";
 
@@ -24,86 +25,63 @@ const DepartmentPositionSubFormComponent = ({
   return (
     <FieldArray name={prefix || "department_positions"}>
       {({ push, remove }) => (
-        <FullWidth>
-          <div className="space-y-4   w-full">
-            <div className="space-y-4">
-              {values.map((_, index) => (
-                <div
-                  key={index}
-                  className="border  rounded-lg p-3 relative w-full"
-                >
-                  <div className="flex justify-between items-center mb-3">
-                    <h4 className="font-medium text-gray-700"></h4>
-                    {values.length > 1 && (
-                      <>
-                        <Button
-                          value="Remove"
-                          variant="secondary"
-                          type="button"
-                          onClick={() => remove(index)}
-                        />
-                        <button
-                          type="button"
-                          onClick={() => {
-                            remove(index);
-                          }}
-                          className="text-red-600 hover:text-red-800 text-sm"
-                          disabled={disabled}
-                        >
-                          Remove
-                        </button>
-                      </>
-                    )}
-                  </div>
-
-                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <Field
-                      component={FormikSelectField}
-                      label="Ministry/Department"
-                      id={`department_positions.${index}.department_name`}
-                      name={`department_positions.${index}.department_name`}
-                      placeholder="Select department"
-                      options={departmentsOptions || []}
+        <>
+          {values.map((_, index) => (
+            <Fragment key={index}>
+              <>
+                {values.length > 1 && (
+                  <FullWidth $justify="right">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        remove(index);
+                      }}
+                      className="text-red-600 hover:text-red-800 text-sm"
                       disabled={disabled}
-                    />
+                    >
+                      Remove
+                    </button>
+                  </FullWidth>
+                )}
+              </>
 
-                    <Field
-                      component={FormikSelectField}
-                      label="Position"
-                      id={`department_positions.${index}.position_name`}
-                      name={`department_positions.${index}.position_name`}
-                      placeholder="Select position"
-                      options={positionsOptions || []}
-                      disabled={disabled}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
+              <>
+                <Field
+                  component={FormikSelectField}
+                  label="Ministry/Department"
+                  id={`department_positions.${index}.department_id`}
+                  name={`department_positions.${index}.department_id`}
+                  placeholder="Select department"
+                  options={departmentsOptions || []}
+                  disabled={disabled}
+                />
 
-            <div className="flex justify-end w-full">
-              <Button
-                variant="ghost"
-                type="button"
-                onClick={() => {
-                  push({ department_name: "", position_name: "" });
-                }}
-                disabled={disabled}
-                value=" Add Another Ministry/Department & Position"
-              />
-              {/* <Button
-                            variant="ghost"
-                              type="button"
-                              onClick={() => {
-                                push({ department_name: "", position_name: "" });
-                                addDepartmentPosition();
-                              }}
-                              disabled={disabled}
-                              value=" Add Another Ministry/Department & Position"
-                            /> */}
-            </div>
-          </div>
-        </FullWidth>
+                <Field
+                  component={FormikSelectField}
+                  label="Position"
+                  id={`department_positions.${index}.position_id`}
+                  name={`department_positions.${index}.position_id`}
+                  placeholder="Select position"
+                  options={positionsOptions || []}
+                  disabled={disabled}
+                />
+                {index !== values.length - 1 && <HorizontalLine />}
+              </>
+            </Fragment>
+          ))}
+
+          <FullWidth $justify="right">
+            <Button
+              variant="ghost"
+              type="button"
+              onClick={() => {
+                push({ department_id: "", position_id: "" });
+              }}
+              disabled={disabled}
+              value=" Add Another Ministry/Department & Position"
+            />
+          </FullWidth>
+        </>
       )}
     </FieldArray>
   );
