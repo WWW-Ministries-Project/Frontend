13688413a4a470753ff2499ee4f8d535e28f1e3b
Saah Asiedu <saah.asiedu@amalitech.org>
2025-07-04 00:16:59 +0000

    added empty state

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const EmptyState = (props) => {
     return (
-        <div className={`${props.className}`}>
+        <div className={`text-center py-8 w-1/4 mx-auto ${props.className}`}>
             {/* <h1>EmptyState</h1> */}
             <img src={empty} alt="" />
             <div className={`text-center text-primary my-5 H400`}>{props.msg}</div>
