649e6b7852a1f62278c78a9f7dc7529695034825
Benjamin Nyankson <nyanksonbenjamin4@gmail.com>
2025-07-20 22:40:41 +0000

    create market

--- src/pages/HomePage/pages/MarketPlace/MarketPlace.tsx
+++ src/pages/HomePage/pages/MarketPlace/MarketPlace.tsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 import { HeaderControls } from "@/components/HeaderControls";
-import PageOutline from "../../Components/PageOutline";
+import { Modal } from "@/components/Modal";
 import { useFetch } from "@/CustomHooks/useFetch";
+import { usePost } from "@/CustomHooks/usePost";
 import { api } from "@/utils";
-import GridComponent from "../../Components/reusable/GridComponent";
+import { showNotification } from "@/pages/HomePage/utils/helperFunctions";
 import { MarketCard } from "./components/MarketCard";
+import { AddMarketForm } from "./components/forms/AddMarketForm";
+import { IMarket } from "@/utils/api/marketPlace/interface";
+import PageOutline from "../../Components/PageOutline";
+import GridComponent from "../../Components/reusable/GridComponent";
 
 export function MarketPlace() {
-  const { data: markets } = useFetch(api.fetch.fetchMarkets);
+  const [openModal, setOpenModal] = useState(false);
+
+  const { data: markets, refetch } = useFetch(api.fetch.fetchMarkets);
+  const {
+    postData,
+    data: newMarket,
+    loading: isSubmitting,
+  } = usePost(api.post.createMarket);
+
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+
+  const handleAddMarket = async (market: IMarket) => {
+    const { id, event_id, ...rest } = market;
+
+    try {
+      await postData({ ...rest, event_id: +event_id });
+    } catch (error) {
+      showNotification("Something went wrong", "error");
+    }
+  };
+
+  useEffect(() => {
+    if (newMarket) {
+      showNotification("Market created successfully", "success");
+      refetch();
+      handleCloseModal();
+    }
+  }, [newMarket, refetch]);
 
   return (
     <PageOutline>
@@ -15,25 +49,34 @@ export function MarketPlace() {
         subtitle="Manage your market here"
         screenWidth={window.innerWidth}
         btnName="Create market"
-        handleClick={() => {}}
+        handleClick={handleOpenModal}
       />
 
       <GridComponent
         columns={[]}
         data={markets?.data || []}
         displayedCount={6}
-        filter={""}
+        filter=""
         setFilter={() => {}}
-        renderRow={(row) => (
+        renderRow={({ original }) => (
           <MarketCard
-            market={row.original}
-            key={row.original.id}
+            key={original.id}
+            market={original}
             handleEdit={() => {}}
             handleDelete={() => {}}
             openMarket={() => {}}
           />
         )}
       />
+
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <AddMarketForm
+          onClose={handleCloseModal}
+          editData={null}
+          onSubmit={handleAddMarket}
+          loading={isSubmitting}
+        />
+      </Modal>
     </PageOutline>
   );
 }
