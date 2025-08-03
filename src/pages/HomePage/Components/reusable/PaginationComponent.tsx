39ea32c77c90a5f5bc3cdfd112376e8d5f98bbbb
Saah Asiedu <saah.asiedu@amalitech.org>
2025-08-03 13:57:40 +0000

    pagination to be continued

--- src/pages/HomePage/Components/reusable/PaginationComponent.tsx
+++ src/pages/HomePage/Components/reusable/PaginationComponent.tsx
@@ -1,66 +1,78 @@
-interface Props {
-  canGoBack: boolean;
-  canGoForward: boolean;
-  goBack: () => void;
-  goForward: () => void;
-  goFirst: () => void;
-  goToPage: (page: number) => void;
-  totalPages: number;
+import { usePaginate } from "@/CustomHooks/usePaginate";
+
+interface IProps {
+  total?: number;
+  take: number;
+  // page: number;
+  onPageChange: (newPage: number, take: number) => void;
 }
 
-const PaginationComponent = ({
-  goBack,
-  goForward,
-  canGoBack,
-  canGoForward,
-  goFirst,
-  goToPage,
-  ...rest
-}: Props): JSX.Element => {
+export const PaginationComponent = ({
+  total,
+  take,
+  // page,
+  onPageChange,
+}: IProps) => {
+  const {
+    canGoBack,
+    canGoForward,
+    goBack,
+    goForward,
+    goFirst,
+    goToPage,
+    totalPages,
+    currentPage,
+  } = usePaginate({ total: total || take, take, onPageChange });
+
   return (
-    <div className={`flex justify-end gap-1 text-gray my-2`}>
+    <div className="flex justify-end gap-1 text-gray my-2">
       <button
         onClick={goFirst}
-        className={
+        disabled={!canGoBack}
+        className={`border p-1 rounded-md ${
           canGoBack
-            ? "border border-primary p-1 sm rounded-md text-primary"
-            : "border border-lightGray p-1 rounded-md opacity-50"
-        }
+            ? "border-primary text-primary"
+            : "border-lightGray opacity-50"
+        }`}
       >
         <span className="hidden sm:inline">First page</span>
         <span className="inline sm:hidden text-xs">First</span>
       </button>
+
       <button
-        className={
-          canGoBack
-            ? "border border-primary p-1 rounded-md text-primary"
-            : "border border-lightGray p-1 rounded-md opacity-50"
-        }
+        onClick={goBack}
         disabled={!canGoBack}
-        onClick={() => goBack()}
+        className={`border p-1 rounded-md ${
+          canGoBack
+            ? "border-primary text-primary"
+            : "border-lightGray opacity-50"
+        }`}
       >
         <span className="hidden sm:inline">Previous page</span>
         <span className="inline sm:hidden text-xs">Previous</span>
       </button>
+
       <button
-        className={
-          canGoForward
-            ? "border border-primary p-1 rounded-md text-primary"
-            : "border border-lightGray p-1 rounded-md opacity-50"
-        }
+        onClick={goForward}
         disabled={!canGoForward}
-        onClick={() => goForward()}
+        className={`border p-1 rounded-md ${
+          canGoForward
+            ? "border-primary text-primary"
+            : "border-lightGray opacity-50"
+        }`}
       >
         <span className="hidden sm:inline">Next page</span>
         <span className="inline sm:hidden text-xs">Next</span>
       </button>
+
       <button
-        className={
+        onClick={() => goToPage(totalPages)}
+        disabled={!canGoForward}
+        className={`border p-1 rounded-md ${
           canGoForward
-            ? "border border-primary p-1 rounded-md text-primary"
-            : "border border-lightGray p-1 rounded-md opacity-50"
-        }
-        onClick={() => goToPage(rest.totalPages - 1)}
+            ? "border-primary text-primary"
+            : "border-lightGray opacity-50"
+        }`}
       >
         <span className="hidden sm:inline">Last page</span>
         <span className="inline sm:hidden text-xs">Last</span>
@@ -68,5 +80,3 @@ const PaginationComponent = ({
     </div>
   );
 };
-
-export default PaginationComponent;
