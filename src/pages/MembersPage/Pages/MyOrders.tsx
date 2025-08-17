3c51eda2d1a1ca1c7a319d136872e1c3671e4ac2
Benjamin Nyankson <nyanksonbenjamin4@gmail.com>
2025-08-17 17:51:11 +0000

    refactor: checkout-form

--- src/pages/MembersPage/Pages/MyOrders.tsx
+++ src/pages/MembersPage/Pages/MyOrders.tsx
@@ -1,3 +1,4 @@
+import EmptyState from "@/components/EmptyState";
 import TableComponent from "@/pages/HomePage/Components/reusable/TableComponent";
 import { ICartItem } from "@/utils";
 import { ColumnDef } from "@tanstack/react-table";
@@ -49,23 +50,30 @@ export const MyOrders = () => {
       {
         header: "Payment",
         accessorKey: "payment_status",
+        cell: ({ row }) => {
+          return getStatusBadge(row.original.payment_status);
+        },
       },
       {
         header: "Delivery",
         accessorKey: "delivery_status",
+        cell: ({ row }) => {
+          return getStatusBadge(row.original.delivery_status);
+        },
       },
     ],
     []
   );
   return (
-    <div className="">
+    <>
       <TableComponent
         columns={tableColumns}
-        data={dummyOrders}
+        data={[]}
         displayedCount={10}
         className="relative"
       />
-    </div>
+      <EmptyState msg="No Orders made yet" />
+    </>
   );
 };
 
@@ -74,19 +82,25 @@ interface MyOrders extends ICartItem {
   delivery_status: "pending" | "delivered" | "cancelled";
 }
 
-const dummyOrders: MyOrders[] = [
-  {
-    name: "T-shirt",
-    payment_status: "pending",
-    delivery_status: "pending",
-    id: "1",
-    price_amount: 100,
-    price_currency: "",
-    quantity: 10,
-    product_type: "Shirt",
-    product_category: "Adults",
-    image_url: "",
-    color: "Red",
-    size: "XL",
-  },
-];
+
+const getStatusColor = (status: string) => {
+  switch (status) {
+    case "pending":
+      return "bg-[#EAECF0]";
+    case "success":
+    case "delivered":
+      return "bg-green-500 text-white";
+
+    default:
+      return "bg-red-500 text-white";
+  }
+};
+
+export const getStatusBadge = (status: string) => {
+  const color = getStatusColor(status);
+  return (
+    <span className={`inline-block px-2 py-1 rounded ${color}`}>
+      {status.charAt(0).toUpperCase() + status.slice(1)}
+    </span>
+  );
+};
